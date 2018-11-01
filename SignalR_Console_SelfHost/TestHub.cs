d5eaf8fb9e99c0cf151afc68994420e16c430a60
Siva <sivaeedala@gmail.com>
2018-11-01 22:33:17 +0530

    Add project files.

---
+++ SignalR_Console_SelfHost/TestHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_Console_SelfHost
+{
+    public class TestHub:Hub
+    {      
+
+        public void SendMessage(string message)
+        {
+            Clients.All.PushMessage(message);
+        }
+    }
+}
