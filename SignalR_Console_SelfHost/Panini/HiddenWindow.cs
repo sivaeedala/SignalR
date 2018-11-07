2bb8ea9fe0c0797f18f5e5802bc6f1e57646ca58
Siva <sivaeedala@gmail.com>
2018-11-07 23:21:30 +0530

    Added Hidden Window

---
+++ SignalR_Console_SelfHost/Panini/HiddenWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalR_Console_SelfHost.Panini
+{
+    public partial class HiddenWindow : Form
+    {
+        public HiddenWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
